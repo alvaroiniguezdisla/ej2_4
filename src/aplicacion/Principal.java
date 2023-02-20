534565db2b75ad7a1bad1aa624cf26ed60f0bb83
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:14:20 +0100

    ej4 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_4;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        int i;
+        int [] lista= {1,2,4,6,9,7,23,89};
+        System.out.println(ej2_4.sumalista(lista, i=lista.length-1));
+
+
+    }
 }
--- src/dominio/ej2_4.java
+++ src/dominio/ej2_4.java
@@ -1,2 +1,29 @@
-package dominio;public class ej2_4 {
+package dominio;
+
+public class ej2_4 {
+
+
+
+
+    public static int sumalista( int[] lista,int i) {
+        if(i==0) {
+            return lista[0];
+        }else {
+            return lista[i]+sumalista(lista,i-1);
+
+
+        }
+    }
+
+    public static int sumaMem(int[]T,int i,int s) {
+        if(i==-1) {
+            return s;
+        }else {
+            return sumaMem(T,i-1,s+T[i]);
+        }
+    }
+
+
+
 }
+
